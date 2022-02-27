805e39e46baa2c0ee568bc50ef972cfcf97cb128
hoxmark <bhoxmark@gmail.com>
2022-02-27 08:05:32 +0100

    Initial Commit

---
+++ FlagGuesser/FlagGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuesserApp.swift
+//  FlagGuesser
+//
+//  Created by Bjørn Hoxmark on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
